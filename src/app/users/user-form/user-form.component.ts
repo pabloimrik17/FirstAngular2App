3cf407b0e83bef2f812552e53a7f84adfa1c0419
Etherless <pabloimrik17@gmail.com>
2017-07-05 07:17:57 +0200

    User Form Component: View, style

---
+++ src/app/users/user-form/user-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { User } from '../../shared/models/user';
+
+@Component({
+  selector: 'user-form',
+  templateUrl: './user-form.component.html',
+  styleUrls: ['./user-form.component.css']
+})
+export class UserFormComponent {
+
+}
